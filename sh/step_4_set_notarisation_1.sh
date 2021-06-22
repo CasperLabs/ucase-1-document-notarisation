8b6be0e35b973be3513311e56a343d2321651196
Mark A. Greenslade <mark@casperlabs.io>
2021-06-22 17:28:24 +0200

    1. Updated deploy files to reflect the multi-sig.

--- sh/step_4_set_notarisation_1.sh
+++ sh/step_4_set_notarisation_1.sh
@@ -1,14 +1,15 @@
 _PATH_TO_CLIENT=$(get_path_to_client)
 
-
 function _main()
 {
-    local USER1_SECRET_KEY=$(get_path_to_secret_key "$NCTL_ACCOUNT_TYPE_USER" "1")
-    local PATH_TO_DEPLOY="$_PATH_TO_DEMO/outputs/notarisation-1.json"
     local CONTRACT_HASH=$(cat $_PATH_TO_DEMO/outputs/contract-hash)
     local DOCUMENT_CHECKSUM=$(cat $_PATH_TO_DEMO/outputs/document-checksum)
     local DOCUMENT_METADATA=$(cat $_PATH_TO_DEMO/outputs/document-metadata)
-    
+    local PATH_TO_DEPLOY="$_PATH_TO_DEMO/outputs/notarisation-1.json"
+    local USER1_SECRET_KEY=$(get_path_to_secret_key "$NCTL_ACCOUNT_TYPE_USER" "1")
+
+    echo $USER1_SECRET_KEY
+
     $_PATH_TO_CLIENT make-deploy \
         --output "$PATH_TO_DEPLOY" \
         --chain-name "casper-net-1" \
--- sh/step_4_set_notarisation_2.sh
+++ sh/step_4_set_notarisation_2.sh
@@ -2,13 +2,29 @@ _PATH_TO_CLIENT=$(get_path_to_client)
 
 function _main()
 {
+    local CONTRACT_HASH=$(cat $_PATH_TO_DEMO/outputs/contract-hash)
+    local DOCUMENT_CHECKSUM=$(cat $_PATH_TO_DEMO/outputs/document-checksum)
+    local DOCUMENT_METADATA=$(cat $_PATH_TO_DEMO/outputs/document-metadata)
+    local PATH_TO_DEPLOY="$_PATH_TO_DEMO/outputs/notarisation-2.json"
+    local USER1_SECRET_KEY=$(get_path_to_secret_key "$NCTL_ACCOUNT_TYPE_USER" "1")
     local USER2_SECRET_KEY=$(get_path_to_secret_key "$NCTL_ACCOUNT_TYPE_USER" "2")
-    local PATH_TO_DEPLOY_IN="$_PATH_TO_DEMO/outputs/notarisation-1.json"
-    local PATH_TO_DEPLOY_OUT="$_PATH_TO_DEMO/outputs/notarisation-2.json"
+
+    $_PATH_TO_CLIENT make-deploy \
+        --output "$PATH_TO_DEPLOY" \
+        --chain-name "casper-net-1" \
+        --gas-price "10" \
+        --payment-amount "10000000000000" \
+        --ttl "1day" \
+        --secret-key "$USER1_SECRET_KEY" \
+        --session-hash "$CONTRACT_HASH" \
+        --session-entry-point "store_string" \
+        --session-arg "$(get_cl_arg_string 'name' "$DOCUMENT_CHECKSUM")" \
+        --session-arg "$(get_cl_arg_string 'value' "$DOCUMENT_METADATA")" \
+        > /dev/null 2>&1
 
     $_PATH_TO_CLIENT sign-deploy \
-        --input "$PATH_TO_DEPLOY_IN" \
-        --output "$PATH_TO_DEPLOY_OUT" \
+        --input "$PATH_TO_DEPLOY" \
+        --output "$PATH_TO_DEPLOY" \
         --secret-key "$USER2_SECRET_KEY" \
         > /dev/null 2>&1
 }
